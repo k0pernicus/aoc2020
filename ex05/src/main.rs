15278513f5ef97d9851ea6863f759d9d2a4f3c00
k0pernicus <>
2020-12-05 16:34:06 +0100

    Added commandline

--- ex05/src/main.rs
+++ ex05/src/main.rs
@@ -1,6 +1,6 @@
 extern crate aoc_helper;
 
-use aoc_helper::file;
+use aoc_helper::{commandline, file};
 use compute::{get_seat_id, PlanePosition};
 use std::{cmp::max, process};
 
@@ -8,7 +8,12 @@ mod lib;
 use lib::compute;
 
 fn main() {
-    let r = file::get_content::<String>("input");
+    // Helper to initialize a CLI
+    let main_app = commandline::AOCApp::new("ex05", "0.1.0", "k0pernicus");
+    let args = main_app.build();
+    let filename = args.get_input_filename();
+
+    let r = file::get_content::<String>(filename.unwrap());
     if let Err(error) = r {
         println!("Got an error when reading input file: {}", error);
         process::exit(1);

f053be21ffe67ad11453ac5a7ed0b1b1b3fe4c59
k0pernicus <>
2020-12-05 10:56:20 +0100

    Added exercise 05

---
+++ ex05/src/lib/compute.rs
@@ -0,0 +1,90 @@
+use std::ops;
+
+#[derive(Copy, Clone)]
+pub struct Range(pub u32, pub u32);
+
+#[derive(Debug)]
+pub struct PlanePosition(pub u32, pub u32);
+
+impl ops::Add for Range {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Range(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+#[derive(Clone, Copy)]
+#[repr(u8)]
+enum RowLetter {
+    Front,
+    Back,
+    Right,
+    Left,
+}
+
+impl Into<char> for RowLetter {
+    fn into(self) -> char {
+        self as u8 as char
+    }
+}
+
+impl RowLetter {
+    pub fn from(c: char) -> Result<RowLetter, ()> {
+        match c {
+            'F' => Ok(RowLetter::Front),
+            'B' => Ok(RowLetter::Back),
+            'R' => Ok(RowLetter::Right),
+            'L' => Ok(RowLetter::Left),
+            _ => Err(()),
+        }
+    }
+    pub fn compute_next_step(&self, range: &Range) -> Range {
+        let mid = (range.1 + range.0) / 2;
+        match self {
+            RowLetter::Back | RowLetter::Right => return Range(mid, range.1),
+            RowLetter::Front | RowLetter::Left => return Range(range.0, mid),
+        }
+    }
+}
+
+pub fn get_plane_row(
+    initial_row_range: &Range,
+    initial_position_range: &Range,
+    indication: &str,
+) -> Option<PlanePosition> {
+    if indication.len() != 10 {
+        return None;
+    }
+    let indication_iter = indication.chars();
+    let row = indication_iter
+        .clone()
+        .take(7)
+        .fold(*initial_row_range, |row, c| {
+            RowLetter::from(c).unwrap().compute_next_step(&row)
+        });
+    let position = indication_iter
+        .skip(7)
+        .fold(*initial_position_range, |position, c| {
+            RowLetter::from(c).unwrap().compute_next_step(&position)
+        });
+    Some(PlanePosition(row.0, position.1))
+}
+
+pub fn get_seat_id(plane_position: &PlanePosition) -> u32 {
+    plane_position.0 * 8 + plane_position.1
+}
+
+/// get_missing_seat_id assumes that seat_ids is already sorted!
+pub fn get_missing_seat_id(seat_ids: &Vec<u32>) -> Option<u32> {
+    let nb_seats = seat_ids.len();
+    for (index, seat_id) in seat_ids.iter().enumerate() {
+        if index >= (nb_seats - 1) {
+            break;
+        }
+        if seat_ids[index + 1] == seat_id + 2 {
+            return Some(seat_id + 1);
+        }
+    }
+    None
+}
---
+++ ex05/src/main.rs
@@ -0,0 +1,51 @@
+extern crate aoc_helper;
+
+use aoc_helper::file;
+use compute::{get_seat_id, PlanePosition};
+use std::{cmp::max, process};
+
+mod lib;
+use lib::compute;
+
+fn main() {
+    let r = file::get_content::<String>("input");
+    if let Err(error) = r {
+        println!("Got an error when reading input file: {}", error);
+        process::exit(1);
+    };
+    let v = r.unwrap();
+    if v.len() == 0 {
+        println!("Input file is empty");
+        process::exit(0);
+    }
+    let initial_row_range = compute::Range(0, 128);
+    let initial_position_range = compute::Range(0, 7);
+
+    // Part I
+    let mut seat_ids = v
+        .iter()
+        .map(|indication| {
+            let plane_position = compute::get_plane_row(
+                &initial_row_range,
+                &initial_position_range,
+                indication.as_str(),
+            )
+            .unwrap_or(PlanePosition(0, 0));
+            return compute::get_seat_id(&plane_position);
+        })
+        .collect::<Vec<u32>>();
+    let max_seat_it = seat_ids.iter().max();
+    if let Some(seat_id) = max_seat_it {
+        println!("The max seat ID is {}", seat_id);
+    } else {
+        println!("There is no max seat ID... bug somewhere :/");
+    }
+
+    // Part II
+    seat_ids.sort();
+    if let Some(missing_seat_id) = compute::get_missing_seat_id(&seat_ids) {
+        println!("The missing seat ID is {}", missing_seat_id);
+    } else {
+        println!("There is no missing seat ID... bug somewhere :/");
+    }
+}
